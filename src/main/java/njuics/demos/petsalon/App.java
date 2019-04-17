ee191fb1a1567f455ce70cb99cb44402834bdb29
jammonkeyyy <1538852806@qq.com>
2019-04-18 00:19:35 +0800

    实现通过controller在数据库与model之间发送消息

--- src/main/java/njuics/demos/petsalon/App.java
+++ src/main/java/njuics/demos/petsalon/App.java
@@ -1,10 +1,12 @@
 package njuics.demos.petsalon;
-
+import javax.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@Entity
 @SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
